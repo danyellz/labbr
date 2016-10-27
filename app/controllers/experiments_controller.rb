db7f4b09b70fc7bbcc02f8b3e84bf28e2dafe89a
Christine Casebolt <christine.casebolt@gmail.com>
2016-10-27 13:49:17 -0700

    adds experiment show page

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,22 +1,28 @@
 class ExperimentsController < ApplicationController
-
+  def show
+    p '*'*80
+    p params
+      p '*'*80
+    @experiment = Experiment.find(params[:id])
+    #Shows specific experiment as well as observations specific to exp.
+  end
   def new
+    @experiment = Experiment.new
     #Give a variable a placeholder form @experiment
   end
 
+    def edit
+      # @experiment = Experiment.find(params[:id])
+      #Meh
+    end
+
   def create
+    #will need to pass proposal id associated with the new experiment, then save it do the DB
     #Create an experiment
   end
 
-  def show
-    #Shows specific experiment as well as observations specific to exp.
-  end
+  def destroy
 
-  def edit
-    #Meh
-  end
-
-  def delete
     #Meh
   end
 end
