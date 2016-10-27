8bbb76a4c6b8abdbb49a9c85765cb9023ac48049
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-10-27 15:49:59 -0700

    update seed file and create views for proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,10 +2,11 @@ class ProposalsController < ApplicationController
 
   def new
     #Give a variable a placeholder form @proposal
+    @proposal = Proposal.new
   end
 
   def index
-    #List all proposals in da DB
+    @proposals = Proposal.all
   end
 
   def create
@@ -14,13 +15,26 @@ class ProposalsController < ApplicationController
 
   def show
     #Lists specific proposal, along with all experiments associated
+    @proposal = Proposal.find(params[:id])
+    @experiments = @proposal.experiments
+    @comments = @proposal.comments
   end
 
   def edit
     #Status stuff
+    @proposal = Proposal.find(params[:id])
   end
 
   def delete
     #Remove it
+    @proposal = Proposal.find(params[:id])
+    @proposal.destroy
+ 
+    redirect_to proposals_path
   end
+
+  private
+    def proposal_params
+      params.require(:proposal).permit(:title, :summary, :hypothesis, :status, )
+    end
 end
