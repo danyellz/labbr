8f444dc44e9f33fb84db4965bc0581a4ddd41d90
Ty Daniels <ty.multisonus@gmail.com>
2016-10-27 16:35:59 -0700

    Adds auth methods

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,22 +1,36 @@
 class ExperimentsController < ApplicationController
 
-  def new
-    #Give a variable a placeholder form @experiment
-  end
-
-  def create
-    #Create an experiment
-  end
-
-  def show
-    #Shows specific experiment as well as observations specific to exp.
-  end
-
-  def edit
-    #Meh
-  end
-
-  def delete
-    #Meh
+  class UsersController < ApplicationController
+ 
+    def new
+     @user = User.new
+    end
+  
+    def create
+     puts "Hit create route!"
+     @user = User.new(user_params)
+     if @user.save
+       puts "Saved things!"
+       redirect_to proposals_path
+     else
+       @errors = @user.errors.full_messages
+     end
+   end
+ 
+   def login 
+     puts "Hit login route!"
+     redirect_to proposals_path 
+    end
+  
+    def destroy
+     redirect_to root
+    end
+  
+ private
+ 
+    def user_params
+     params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+ 
   end
 end
