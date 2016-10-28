cda98db49bb3488e37af37c4855f0b265af75c55
Ty Daniels <ty.multisonus@gmail.com>
2016-10-27 17:10:34 -0700

    Resets authentication

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,25 +2,41 @@ class ProposalsController < ApplicationController
 
   def new
     #Give a variable a placeholder form @proposal
+    @proposal = Proposal.new
   end
 
   def index
     #List all proposals in da DB
+    @proposals = Proposal.all
   end
 
   def create
-    #Add a new proposal
+    #Add a new proposal w/ proposal_params
+
   end
 
   def show
     #Lists specific proposal, along with all experiments associated
+    @proposal = Proposal.find(params[:id])
+    @experiments = @proposal.experiments
+    @comments = @proposal.comments
   end
 
   def edit
     #Status stuff
+    @proposal = Proposal.find(params[:id])
   end
 
   def delete
     #Remove it
+    @proposal = Proposal.find(params[:id])
+    @proposal.destroy
+ 
+    redirect_to proposals_path
   end
+
+   private
+    def proposal_params
+      params.require(:proposal).permit(:title, :summary, :hypothesis, :status, )
+    end
 end
